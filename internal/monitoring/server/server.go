864cb025b3bda6a4a8f367e6674538db7fcc5e58
ahd <ali@aheydari.ir>
2021-09-14 11:33:45 +0430

    handle closed channel

--- internal/monitoring/server/server.go
+++ internal/monitoring/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/ahd99/urlshortner/internal/monitoring/proto"
@@ -36,8 +37,8 @@ func (server *MonitoringServer) Statistics(req *proto.StatReq, resp proto.Monito
 		logger1.Debug("Monitoring -- statistics -- event received from channel")
 		err := resp.Send(&proto.StatResp{Count: server.requestCount})
 		if err != nil {
-			logger1.Error("Monitoring -- statistics -- Error sending count.", 
-				logger.Int64("count", server.requestCount), 
+			logger1.Error("Monitoring -- statistics -- Error sending count.",
+				logger.Int64("count", server.requestCount),
 				logger.String("err", err.Error()))
 			return err
 		}
@@ -46,41 +47,34 @@ func (server *MonitoringServer) Statistics(req *proto.StatReq, resp proto.Monito
 	}
 }
 
-func (server *MonitoringServer) TranList(req *proto.TranListReq, resp proto.Monitoring_TranListServer) error {
-	logger1.Info("Monitoring -- tranlist -- Connection received")
-	for req := range server.tranChannel {
-		logger1.Debug("Monitoring -- tranlist -- tran received from channel", 
-				logger.String("key", req.key), 
-				logger.String("url", req.url), 
-				logger.String("ip", req.ip))
-		err := resp.Send(&proto.TranListResp{Key: req.key, Url: req.url, Ip: req.ip})
-		if err != nil {
-			logger1.Error("Monitoring -- tranlist -- Error sending tran.", 
-					logger.String("key", req.key), 
-					logger.String("url", req.url), 
-					logger.String("ip", req.ip), 
-					logger.String("err", err.Error()))
-			return err
-		}
-		logger1.Debug("Monitoring -- tranlist -- tran sent to client successfully", 
-				logger.String("key", req.key), 
-				logger.String("url", req.url), 
-				logger.String("ip", req.ip))
-	}
-	logger1.Info("Monitoring -- tranlist -- Connection Terminates")
-	return nil
-}
+var tranListConnId_debug int = 0
 
-func GetServer() *MonitoringServer {
-	return server
+func (server *MonitoringServer) TranList(req *proto.TranListReq, resp proto.Monitoring_TranListServer) error {
+	tranListConnId_debug++
+	connId := tranListConnId_debug
+	logger1.Info("Monitoring -- Reqlist(" + strconv.Itoa(connId) + ") -- Connection received.")
+
+	for {
+		select {
+		case req := <-server.tranChannel:
+			logger1.Debug("Monitoring -- Reqlist("+strconv.Itoa(connId)+") -- Req received from channel", logger.String("key", req.key), logger.String("url", req.url), logger.String("ip", req.ip))
+			err := resp.Send(&proto.TranListResp{Key: req.key, Url: req.url, Ip: req.ip})
+			if err != nil {
+				logger1.Error("Monitoring -- Reqlist("+strconv.Itoa(connId)+" ) -- Error sending Req.", logger.String("key", req.key), logger.String("url", req.url), logger.String("ip", req.ip), logger.String("err", err.Error()))
+				server.tranChannel <- req
+				return err
+			}
+			logger1.Debug("Monitoring -- Reqlist("+strconv.Itoa(connId)+") -- REq sent to client successfully", logger.String("key", req.key), logger.String("url", req.url), logger.String("ip", req.ip))
+		case <-resp.Context().Done():
+			logger1.Debug("Monitoring -- Reqlist("+strconv.Itoa(connId)+") -- Channel was done (closed).",
+				logger.String("err", resp.Context().Err().Error()))
+			return resp.Context().Err()
+		}
+	}
 }
 
 func RequestReceived(key string, url string, ip string) {
-	logger1.Debug("Monitoring. Request received start.", 
-		logger.String("key", key), 
-		logger.String("url", url), 
-		logger.String("ip", ip),
-		logger.Int("count", int(server.requestCount)))
+	//logger1.Debug("Monitoring. Request received start.", logger.String("key", key), logger.String("url", url), logger.String("ip", ip), logger.Int("count", int(server.requestCount)))
 	server.requestCount++ //TODO: needs lock.
 	select {
 	case server.statChannel <- true:
@@ -93,8 +87,12 @@ func RequestReceived(key string, url string, ip string) {
 		logger1.Debug("tran sent to tranchannel")
 	default:
 	}
-	logger1.Debug("Monitoring. Request received finish.")
+	//logger1.Debug("Monitoring. Request received finish.")
 
+}
+
+func GetServer() *MonitoringServer {
+	return server
 }
 
 func SetLogger(log logger.Logger) {
